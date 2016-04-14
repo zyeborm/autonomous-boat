73c2092aee108723f786917c6516ea26d21f19a2
jake anderson <yahoo@vapourforge.com>
2016-04-14 18:30:34 +1000

    boat_executive starting

--- sail/Firmware/boat_executive.py
+++ sail/Firmware/boat_executive.py
@@ -1,8 +1,42 @@
 from __future__ import division
 from boat_handling import boat_handling
-from sensors import sensors
+import time
+import logging
+
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
 
 
-boat_handler = boat_handling()
+#from sensors import sensors
 
 
+
+class boat_executive(object):
+    def __init__(self, Operation_Mode):
+        self.Operation_Mode = Operation_Mode
+        boat_handler = boat_handling()  
+        logging.warning('Init boat_executive successfull')        
+        
+
+    def Terminate_Executive(self):
+        """Cleanup the sensors and actuators"""
+        logging.warning('Shutting Down')
+        pass
+        
+    def Main_Loop(self):
+        """Run the main loop"""
+        logging.warning('Main_Loop starting')            
+        while (True):
+            time.sleep(.1)
+
+              
+if __name__ == "__main__":
+    """We are running the real boat"""
+    
+    try:
+      logging.warning('Running in Physical mode')        
+      boat = boat_executive("Physical")
+      boat.Main_Loop()
+    except KeyboardInterrupt:
+      logging.warning('Caught Ctrl+C')
+      boat.Terminate_Executive()
+      
--- sail/Firmware/rotary_read.py
+++ sail/Firmware/rotary_read.py
@@ -3,15 +3,16 @@ import spidev
 
 
 
-spi = spidev.SpiDev()
-spi.open(0,0)
+spi0 = spidev.SpiDev()
+spi0.open(0,0)
 
-def read_wind():
-    """Returns relative wind direction to hull, 0-360 degrees"""
-    resp = spi.xfer2([0x00,0x00])
-    
-    angle = ((resp[0] << 9) | (resp[1] << 1)) >> 4
-    binary_rep = bin(angle).replace('b','')
+spi1 = spidev.SpiDev()
+spi1.open(0,1)
+def _convert_to_angle(raw_data):
+    """Converts 2 byte input to an angle in degrees"""
+
+    angle = ((raw_data[0] << 9) | (raw_data[1] << 1)) >> 4
+    #binary_rep = bin(angle).replace('b','')
 
 #    first_byte = bin(resp[0]).replace('b','')
 #    first_byte = first_byte.zfill(8)[-8:]
@@ -25,12 +26,21 @@ def read_wind():
     #print "compass %08.2f = angle %08.2f binary %s raw %s" % (compass,angle,binary_rep.zfill(16), resp)
     #time.sleep(.1)
     return compass
+
+def read_wind():
+    """Returns relative wind direction to hull, 0-360 degrees"""
+    resp = spi0.xfer2([0x00,0x00])
+    return _convert_to_angle(resp)
+    
+def read_sail():
+    """Returns absolute sail angle to hull, 0-360 degrees"""
+    resp = spi1.xfer2([0x00,0x00])
+    return _convert_to_angle(resp)
     
 if __name__ == "__main__":
-  """loops at 10hz spitting out rotary angle"""
+  """loops at 10hz spitting out rotary angles"""
   import time
   while True:
-    print read_wind()
+    print "wind : %3.2f   sail %3.2f" % (read_wind(), read_sail())
     time.sleep(.1)
-    
     
